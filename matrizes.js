6e0e1be594c648cfef271e28217d2a6bd3f7d873
wangfeyen <wangfeyen@gmail.com>
2023-09-02 15:12:44 -0300

    ok

---
+++ matrizes.js
@@ -0,0 +1,16 @@
+/*Nesta atividade, você deve criar um código em JavaScript 
+com uma matriz bidimensional para armazenar informações sobre animais 
+e as suas características.
+
+Cada linha deve representar um animal e cada coluna terá as diferentes 
+informações sobre ele, como nome, espécie e idade.*/
+let nomes=["Pipoca","Mia","Mimosa","Charles"];
+let especie=["Cachorro","Gato","Vaca","Cavalo"];
+let idade=[6,10,7,22];
+
+let animais=[];
+animais.push(nomes);
+animais.push(especie);
+animais.push(idade);
+
+console.log(animais);
